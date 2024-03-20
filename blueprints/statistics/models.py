031d09f4addd05b16aab74b957347f0063edaddb
mloikc <1336515485@qq.com>
2024-03-20 20:21:54 +0800

    1.3 Add Time and Date Analysis

--- blueprints/statistics/models.py
+++ blueprints/statistics/models.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 
 def get_keywords(processed_data):
@@ -23,3 +25,95 @@ def get_keywords(processed_data):
     word_data = [{'text': word, 'frequency': freq} for word, freq in top_word_freq]
 
     return word_data
+
+
+def process_database_timedata(danmu_type, danmu_data):
+    DATABASE_URI = 'mysql://root:123456@localhost:3306/barrage'
+    engine = create_engine(DATABASE_URI)
+
+    # 创建数据库会话
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # 根据不同的 danmu_type 构建不同的 SQL 查询语句
+    if danmu_type == 'Beg':
+        query = session.query(text("time FROM danmu_data WHERE title = :data")).params(data=danmu_data)
+    elif danmu_type == 'Int':
+        query = session.query(text("time FROM fulldanmu_data WHERE bvid = :data")).params(data=danmu_data)
+    elif danmu_type == 'Adv':
+        query = session.query(text("progress FROM rawdanmu_data WHERE bvid = :data")).params(data=danmu_data)
+    else:
+        # 如果 danmu_type 不在预期的范围内，返回查询错误信息
+        error_message = 'Invalid danmu_type'
+        return {'error': error_message}
+
+    # 执行查询并获取结果
+    contents = query.all()
+
+    # 关闭数据库会话
+    session.close()
+
+    processed_data = []
+    for content in contents:
+        if danmu_type == 'Beg':
+            # 分割时间字符串
+            minutes, seconds = content[0].split(":")
+            # 将小时和分钟转换为整数
+            minutes = int(minutes)
+            seconds = int(seconds)
+
+            # 计算总的分钟数和对应的秒数
+            time_data = minutes * 60 + seconds
+        elif danmu_type == 'Int':
+            time_data = content[0]
+        elif danmu_type == 'Adv':
+            if isinstance(content[0], (int, float)):
+                time_data = content[0] / 1000
+
+        processed_data.append({
+            'time': time_data
+        })
+
+    return processed_data
+
+
+def process_database_datedata(danmu_type, danmu_data):
+    DATABASE_URI = 'mysql://root:123456@localhost:3306/barrage'
+    engine = create_engine(DATABASE_URI)
+
+    # 创建数据库会话
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # 根据不同的 danmu_type 构建不同的 SQL 查询语句
+    if danmu_type == 'Beg':
+        query = session.query(text("date FROM danmu_data WHERE title = :data")).params(data=danmu_data)
+    elif danmu_type == 'Int':
+        query = session.query(text("timestamp FROM fulldanmu_data WHERE bvid = :data")).params(data=danmu_data)
+    elif danmu_type == 'Adv':
+        query = session.query(text("ctime FROM rawdanmu_data WHERE bvid = :data")).params(data=danmu_data)
+    else:
+        # 如果 danmu_type 不在预期的范围内，返回查询错误信息
+        error_message = 'Invalid danmu_type'
+        return {'error': error_message}
+
+    # 执行查询并获取结果
+    contents = query.all()
+
+    # 关闭数据库会话
+    session.close()
+
+    processed_data = []
+    for content in contents:
+        if danmu_type == 'Adv':
+            date_data = content[0]+'000'
+        elif danmu_type == 'Int':
+            date_data = content[0]*1000
+        else:
+            date_data = content[0]  # 因为查询返回的是元组，content 是第一个元素
+
+        processed_data.append({
+            'date': date_data
+        })
+
+    return processed_data
--- blueprints/statistics/views.py
+++ blueprints/statistics/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 
 from blueprints.sentiment.models import process_database_data
-from blueprints.statistics.models import get_keywords
+from blueprints.statistics.models import get_keywords, process_database_timedata, process_database_datedata
 
 stat_bp = Blueprint('stat', __name__)
 
@@ -23,3 +23,36 @@ def analyze_keywords():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@stat_bp.route('/analyze_time', methods=['POST'])
+def analyze_time():
+    try:
+        # 从请求的 JSON 数据中获取 danmuDetail
+        danmu_detail = request.json.get('danmuDetail')
+        if danmu_detail:
+            danmu_type = danmu_detail.get('type')
+            danmu_data = danmu_detail.get('data')
+
+            processed_data = process_database_timedata(danmu_type, danmu_data)
+
+            return jsonify(processed_data)
+        else:
+            return jsonify({'error': 'Invalid request data'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@stat_bp.route('/analyze_date', methods=['POST'])
+def analyze_date():
+    try:
+        # 从请求的 JSON 数据中获取 danmuDetail
+        danmu_detail = request.json.get('danmuDetail')
+        if danmu_detail:
+            danmu_type = danmu_detail.get('type')
+            danmu_data = danmu_detail.get('data')
+
+            processed_data = process_database_datedata(danmu_type, danmu_data)
+
+            return jsonify(processed_data)
+        else:
+            return jsonify({'error': 'Invalid request data'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
